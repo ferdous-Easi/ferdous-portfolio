796a248ee40573ebcd3c6e5e7c3e05ac22054711
EASITAL <ferdous.easital@gmail.com>
2024-02-04 14:17:27 +0600

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c149a5c41d6bff81"; }
+/******/ 		__webpack_require__.h = function() { return "fe6b7a99e6d51b91"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
